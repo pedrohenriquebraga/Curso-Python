fe923e8e14c1c70a614848867e94492cd44aae71
Pedro Henrique Santos Braga <62728884+PedroHenrique-Santos@users.noreply.github.com>
2020-04-09 22:53:16 -0300

    Delete .last_tmp.py

--- Mundo 3/Exercícios/ex_108/.last_tmp.py
+++
@@ -1,9 +0,0 @@
-# Modularização para moeda
-
-from ex108 import *
-
-v = float(input("Digite um valor: R$ "))
-print(f"A metade de {moeda(v)} é {moeda(metade(v))}")
-print(f"O dobro de {moeda(v)} é {moeda(dobro(v))}")
-print(f"{moeda(v)} com 10% de aumento fica {moeda(aumentar(v, 10))}")
-print(f"{moeda(v)} com 13% de diminuição fica {moeda(diminuir(v, 13))}")
