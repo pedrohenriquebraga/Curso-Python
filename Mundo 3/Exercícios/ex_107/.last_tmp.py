59879180f75b61539bc3432b1fe52325214a3c9e
Pedro Henrique Santos Braga <62728884+PedroHenrique-Santos@users.noreply.github.com>
2020-04-09 22:52:16 -0300

    Delete .last_tmp.py

--- Mundo 3/Exercícios/ex_107/.last_tmp.py
+++
@@ -1,9 +0,0 @@
-# Modularização para moeda
-
-import moeda
-
-v = float(input("Digite um valor: R$ "))
-print(f"A metade do valor é R$ {moeda.metade(v)}")
-print(f"O dobro do valor é R$ {moeda.dobro(v)}")
-print(f"Com aumento de 10% fica R$ {moeda.aumentar(v, 10)}")
-print(f"Com desconto de 13% fica R$ {moeda.diminuir(v, 13)}")
